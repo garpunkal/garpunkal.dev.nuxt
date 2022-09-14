b9cef5f89332d1c74294ac827fa45da16a3ce059
Gareth Wright <garpunkal@gmail.com>
2022-09-14 21:55:47 +0100

    updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,12 +26,12 @@ export default {
       { name: 'copyright', content: 'Gareth Wright' },
       { name: 'theme-color', content: '#077DA0' },
       { property: "og:type", content: "website" },
-      { property: "og:url", content: "https://garpunkal.dev/" }, 
+      { property: "og:url", content: "https://garpunkal.dev/" },
       { property: "og:title", content: "Gareth Wright - Technical Development Lead - Senior C# .net MVC Developer" },
       { property: "og:description", content: "Technical Development Lead, Senior C# .net MVC Developer & Umbraco Certified Expert." },
       { property: "og:image", content: "https://en.gravatar.com/userimage/11972664/7a04387a8dd8f32161e728bc09686344.jpeg?size=400" },
       { property: "twitter:image", content: "https://en.gravatar.com/userimage/11972664/7a04387a8dd8f32161e728bc09686344.jpeg?size=400" },
-      { property: "twitter:card", content: "summary"},
+      { property: "twitter:card", content: "summary" },
       { property: "twitter:title", content: "Gareth Wright - Technical Development Lead - Senior C# .net MVC Developer" },
       { property: "twitter:url", content: "https://garpunkal.dev/" },
       { property: "twitter:description", content: "Technical Development Lead, Senior C# .net MVC Developer & Umbraco Certified Expert." },
@@ -43,7 +43,11 @@ export default {
       { rel: 'preconnect', href: 'https://www.googletagmanager.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-      { rel: 'preconnect', href: 'https://www.google-analytics.com' }
+      { rel: 'preconnect', href: 'https://www.google-analytics.com' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', sizes: '32x32', type: '"image/png', href: '/favicon-32x32.png' },
+      { rel: 'icon', sizes: '16x16', type: '"image/png', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
 
     ]
   },
@@ -77,7 +81,7 @@ export default {
   sitemap: {
     hostname: "https://garpunkal.dev",
     gzip: true,
-    exclude: ["/admin/"]
+    exclude: [""]
   },
 
   performance: {
@@ -87,10 +91,10 @@ export default {
     {
       UserAgent: 'Googlebot',
       CrawlDelay: 2,
-      Allow: '/' 
+      Allow: '/'
     },
     {
-      UserAgent: '*',    
+      UserAgent: '*',
       CrawlDelay: 10,
       Allow: '/'
     },
