5c233f04fdaa0fa6d7dc630d1dc6d637c92a1d7e
Gareth Wright <garpunkal@gmail.com>
2022-09-14 22:27:37 +0100

    updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ export default {
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', sizes: '32x32', type: '"image/png', href: '/favicon-32x32.png' },
       { rel: 'icon', sizes: '16x16', type: '"image/png', href: '/favicon-16x16.png' },
-      { rel: 'manifest', href: '/site.webmanifest' },
+      
     ]
   },
   css: [
